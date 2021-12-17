ec65ceba542ee3acc3ecbd4b5a2a37a6a648fdd7
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2021-12-17 10:48:06 +0000

    Create 04_multiply.pas

---
+++ 04_multiply.pas
@@ -0,0 +1,19 @@
+     PROGRAM multiply(INPUT,OUTPUT);
+
+     { This program asks for two numbers
+       then multiplies them together }
+
+     VAR
+       first_integer,
+       second_integer,
+       answer : INTEGER;
+
+     BEGIN
+       WRITE('Enter an integer : ');
+       READLN(first_integer);
+       WRITE('Enter another integer : ');
+       READLN(second_integer);
+       
+       answer := first_integer * second_integer;
+       WRITELN('The answer = ',answer:1);
+     END.   { program multiply }
