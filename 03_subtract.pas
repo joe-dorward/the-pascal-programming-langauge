4b7d4684f7281ffab71988c219307e25bb46992b
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2021-12-17 10:52:31 +0000

    Update 03_subtract.pas

--- 03_subtract.pas
+++ 03_subtract.pas
@@ -1,19 +1,19 @@
-     PROGRAM subtract(INPUT,OUTPUT);
+PROGRAM subtract(INPUT,OUTPUT);
 
-     { This program asks for two numbers
-       then subtracts one from the other }
+{ This program asks for two numbers
+  then subtracts one from the other }
 
-     VAR
-       first_integer,
-       second_integer,
-       answer : INTEGER;
+VAR
+  first_integer,
+  second_integer,
+  answer : INTEGER;
 
-     BEGIN
-       WRITE('Enter an integer : ');
-       READLN(first_integer);
-       WRITE('Enter another integer : ');
-       READLN(second_integer);
+BEGIN
+  WRITE('Enter an integer : ');
+  READLN(first_integer);
+  WRITE('Enter another integer : ');
+  READLN(second_integer);
        
-       answer := first_integer - second_integer;
-       WRITELN('The answer = ',answer:1);
-     END.   { program subtract }
+  answer := first_integer - second_integer;
+  WRITELN('The answer = ',answer:1);
+END.   { program subtract }
