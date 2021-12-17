90e8b2a4a67437fcafbcbd78edbcc16bb9934337
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2021-12-17 13:36:42 +0000

    Create 19_for_demo.pas

---
+++ 19_for_demo.pas
@@ -0,0 +1,13 @@
+PROGRAM for_demo(INPUT,OUTPUT);
+
+{ this program demonstrates the simple 
+  use of a FOR loop }
+
+VAR
+  I : INTEGER;
+
+BEGIN
+  FOR I := 1 TO 2 DO
+    WRITELN('The value of I is ',I:0);
+    
+END.   { for_demo }
