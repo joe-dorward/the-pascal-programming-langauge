5595687d488c8d8b3c132cfcf0442b69784db78a
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2021-12-17 10:53:30 +0000

    Update 04_multiply.pas

--- 04_multiply.pas
+++ 04_multiply.pas
@@ -1,19 +1,19 @@
-     PROGRAM multiply(INPUT,OUTPUT);
+PROGRAM multiply(INPUT,OUTPUT);
 
-     { This program asks for two numbers
-       then multiplies them together }
+{ This program asks for two numbers
+  then multiplies them together }
 
-     VAR
-       first_integer,
-       second_integer,
-       answer : INTEGER;
+VAR
+  first_integer,
+  second_integer,
+  answer : INTEGER;
 
-     BEGIN
-       WRITE('Enter an integer : ');
-       READLN(first_integer);
-       WRITE('Enter another integer : ');
-       READLN(second_integer);
+BEGIN
+  WRITE('Enter an integer : ');
+  READLN(first_integer);
+  WRITE('Enter another integer : ');
+  READLN(second_integer);
        
-       answer := first_integer * second_integer;
-       WRITELN('The answer = ',answer:1);
-     END.   { program multiply }
+  answer := first_integer * second_integer;
+  WRITELN('The answer = ',answer:1);
+END.   { program multiply }
