c27c7aebfd9bb5ae58d75dd2eac16f13ec3ebed9
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2021-12-28 22:38:37 +0000

    Create 02_integer_division.pas

---
+++ chapter-07/02_integer_division.pas
@@ -0,0 +1,8 @@
+PROGRAM integer_division(INPUT,OUTPUT);
+
+{ this is a small test program }
+
+BEGIN
+  WRITE('-15 divided by two is: "',-15 DIV 2);
+  WRITELN('" Remainder: "',-15 MOD 2,'"');
+END.   { program integer_division }
