59799b2011a790cc1d4b721acdefd3e253d6e00a
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2021-12-28 16:08:52 +0000

    Create 04_reads_1.pas

---
+++ chapter-03/04_reads_1.pas
@@ -0,0 +1,26 @@
+PROGRAM reads_1(INPUT,OUTPUT);
+
+{ this program demonstrates the difference 
+  between the READ and READLN }
+
+VAR
+  first,
+  second,
+  third,
+  fourth,
+  fifth,
+  sixth : INTEGER;
+
+BEGIN
+
+  WRITE('This is the prompt for the reads : ');
+  READ(first,second,third);
+  WRITELN(first,second,third);
+  
+  WRITE('This is the prompt for the readln : ');
+  READLN(fourth,fifth,sixth);
+  WRITELN(fourth,fifth,sixth);
+
+  WRITE('Press <enter> to continue : ');
+  READLN;
+END.   { program reads_1 }
