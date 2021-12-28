4401b7301e98145a1c1a84c19ce162cd13bf0fcf
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2021-12-28 16:07:32 +0000

    Create 03_writes_1.pas

---
+++ chapter-03/03_writes_1.pas
@@ -0,0 +1,11 @@
+PROGRAM writes_1(INPUT,OUTPUT);
+
+{ this program shows difference between
+  WRITE and WRITELN }
+
+BEGIN
+  WRITELN('================');
+  WRITE;
+  WRITELN;
+  WRITELN('================');
+END.   { program writes_1 }
