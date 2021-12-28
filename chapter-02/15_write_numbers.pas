775712571d77cc919d013b326d860a883c9838db
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2021-12-28 16:03:13 +0000

    Update 15_write_numbers.pas

--- chapter-02/15_write_numbers.pas
+++ chapter-02/15_write_numbers.pas
@@ -1,165 +1,165 @@
-PROGRAM write_numbers(INPUT,OUTPUT);
+     PROGRAM write_numbers(INPUT,OUTPUT);
 
-{
-  This program writes out, in English, any number less than
-  2,000 entered at the prompt.
-}
+     {
+       This program writes out, in English, any number less than
+       2,000 entered at the prompt.
+     }
      
-VAR
-  I,
-  test_number,
-  the_number : INTEGER;
+     VAR
+       I,
+       test_number,
+       the_number : INTEGER;
 
-{ -------------------------------------------------------- }
-PROCEDURE write_units(units : INTEGER);
+     { -------------------------------------------------------- }
+     PROCEDURE write_units(units : INTEGER);
 
-{ this procedure writes out the value of the "units" }
+     { this procedure writes out the value of the "units" }
 
-BEGIN
-  IF units = 9 THEN
-    WRITE('nine')
-      ELSE
-        IF units = 8 THEN
-          WRITE('eight')
-        ELSE
-          IF units = 7 THEN
-            WRITE('seven')
-          ELSE
-            IF units = 6 THEN
-              WRITE('six')
-            ELSE
-              IF units = 5 THEN
-                WRITE('five')
-              ELSE
-                IF units = 4 THEN
-                  WRITE('four')
-                ELSE
-                  IF units = 3 THEN
-                    WRITE('three')
-                  ELSE
-                    IF units = 2 THEN
-                      WRITE('two')
-                    ELSE
-                      IF units = 1 THEN
-                        WRITE('one');
-END;   { procedure write_units }
-{ -------------------------------------------------------- }
-PROCEDURE write_tens(VAR tens : INTEGER);
+     BEGIN
+       IF units = 9 THEN
+         WRITE('nine')
+       ELSE
+         IF units = 8 THEN
+           WRITE('eight')
+         ELSE
+           IF units = 7 THEN
+             WRITE('seven')
+           ELSE
+             IF units = 6 THEN
+               WRITE('six')
+             ELSE
+               IF units = 5 THEN
+                 WRITE('five')
+               ELSE
+                 IF units = 4 THEN
+                   WRITE('four')
+                 ELSE
+                   IF units = 3 THEN
+                     WRITE('three')
+                   ELSE
+                     IF units = 2 THEN
+                       WRITE('two')
+                     ELSE
+                       IF units = 1 THEN
+                         WRITE('one');
+     END;   { procedure write_units }
+     { -------------------------------------------------------- }
+     PROCEDURE write_tens(VAR tens : INTEGER);
 
-{ this procedure writes out the value of the "tens" }
+     { this procedure writes out the value of the "tens" }
 
-BEGIN
-  IF tens > 89 THEN
-    BEGIN
-      WRITE('ninty');
-      tens := tens - 90;
-    END
-  ELSE
-    IF tens > 79 THEN
-      BEGIN
-        WRITE('eighty');
-        tens := tens - 80;
-      END
-    ELSE
-      IF tens > 69 THEN
-        BEGIN
-          WRITE('seventy');
-          tens := tens - 70;
-        END
-      ELSE
-        IF tens > 59 THEN
-          BEGIN
-            WRITE('sixty');
-            tens := tens - 60;
-          END
-        ELSE
-          IF tens > 49 THEN
+     BEGIN
+       IF tens > 89 THEN
+         BEGIN
+           WRITE('ninty');
+           tens := tens - 90;
+         END
+       ELSE
+         IF tens > 79 THEN
+           BEGIN
+             WRITE('eighty');
+             tens := tens - 80;
+           END
+         ELSE
+           IF tens > 69 THEN
+             BEGIN
+               WRITE('seventy');
+               tens := tens - 70;
+             END
+           ELSE
+             IF tens > 59 THEN
+               BEGIN
+                 WRITE('sixty');
+                 tens := tens - 60;
+               END
+             ELSE
+               IF tens > 49 THEN
+                 BEGIN
+                   WRITE('fifty');
+                   tens := tens - 50;
+                 END
+               ELSE
+                 IF tens > 39 THEN
+                   BEGIN
+                     WRITE('forty');
+                     tens := tens - 40;
+                   END
+                 ELSE
+                   IF tens > 29 THEN
+                     BEGIN
+                       WRITE('thirty');
+                       tens := tens - 30;
+                     END
+                   ELSE
+                     IF tens > 19 THEN
+                       BEGIN
+                         WRITE('twenty');
+                         tens := tens - 20;
+                       END
+                     ELSE
+                       IF tens > 15 THEN
+                         BEGIN
+                           write_units(tens);
+                           write('teen');
+                         END;
+     END;    { procedure write_tens } 
+     { -------------------------------------------------------- }
+     PROCEDURE write_high_teens(the_number : INTEGER);
+
+     { this procedure writes out 19 - 16, with a special 
+       case for 18 }
+
+     BEGIN
+       the_number := the_number - 10;
+       write_units(the_number);   { writes out "tens" column }
+
+       { add correct suffix }
+       IF (the_number = 8) THEN { write eighteen rather than eightteen }
+         WRITE('een')
+       ELSE
+         write('teen');
+     END;   { procedure write_high_teens }
+     { -------------------------------------------------------- }
+     PROCEDURE write_low_teens(the_number : INTEGER);
+ 
+     { this procedure writes out special cases 15 - 10 }
+
+     BEGIN
+       IF (the_number = 15) THEN
+         WRITE('fifteen')
+       ELSE
+         IF (the_number = 14) THEN
+           WRITE('fourteen')
+         ELSE
+           IF (the_number = 13) THEN
+             WRITE('thirteen')
+           ELSE
+             IF (the_number = 12) THEN
+                WRITE('twelve')
+             ELSE
+               IF (the_number = 11) THEN
+                 WRITE('eleven')
+               ELSE
+                 IF (the_number = 10) THEN
+                   WRITE('ten');
+     END;   { procedure write_low_teens }
+     { -------------------------------------------------------- }
+     PROCEDURE write_the_number(the_number : INTEGER);
+ 
+     BEGIN
+       the_number := the_number MOD 100;
+ 
+       { if "the_number" is greater than 19 }
+       IF (the_number > 19) THEN
+         BEGIN
+           WRITE('and ');
+           write_tens(the_number);
+           write_units(the_number);
+         END
+       ELSE
+         { if "the_number" 19, 18, 17, 16 }
+         IF (the_number < 20) AND (the_number > 15) THEN
             BEGIN
-              WRITE('fifty');
-              tens := tens - 50;
-            END
-          ELSE
-            IF tens > 39 THEN
-              BEGIN
-                WRITE('forty');
-                tens := tens - 40;
-              END
-            ELSE
-              IF tens > 29 THEN
-                BEGIN
-                  WRITE('thirty');
-                  tens := tens - 30;
-                END
-              ELSE
-                IF tens > 19 THEN
-                  BEGIN
-                    WRITE('twenty');
-                    tens := tens - 20;
-                  END
-                ELSE
-                  IF tens > 15 THEN
-                    BEGIN
-                      write_units(tens);
-                      write('teen');
-                    END;
-END;    { procedure write_tens } 
-{ -------------------------------------------------------- }
-PROCEDURE write_high_teens(the_number : INTEGER);
-
-{ this procedure writes out 19 - 16, with a special 
-  case for 18 }
-
-BEGIN
-  the_number := the_number - 10;
-  write_units(the_number);   { writes out "tens" column }
-
-  { add correct suffix }
-  IF (the_number = 8) THEN { write eighteen rather than eightteen }
-    WRITE('een')
-  ELSE
-    write('teen');
-END;   { procedure write_high_teens }
-{ -------------------------------------------------------- }
-PROCEDURE write_low_teens(the_number : INTEGER);
- 
-{ this procedure writes out special cases 15 - 10 }
-
-BEGIN
-  IF (the_number = 15) THEN
-    WRITE('fifteen')
-  ELSE
-    IF (the_number = 14) THEN
-      WRITE('fourteen')
-    ELSE
-      IF (the_number = 13) THEN
-        WRITE('thirteen')
-      ELSE
-        IF (the_number = 12) THEN
-           WRITE('twelve')
-        ELSE
-          IF (the_number = 11) THEN
-            WRITE('eleven')
-          ELSE
-            IF (the_number = 10) THEN
-              WRITE('ten');
-END;   { procedure write_low_teens }
-{ -------------------------------------------------------- }
-PROCEDURE write_the_number(the_number : INTEGER);
- 
-BEGIN
-  the_number := the_number MOD 100;
- 
-  { if "the_number" is greater than 19 }
-  IF (the_number > 19) THEN
-    BEGIN
-      WRITE('and ');
-      write_tens(the_number);
-      write_units(the_number);
-    END
-  ELSE
-    { if "the_number" 19, 18, 17, 16 }
-    IF (the_number < 20) AND (the_number > 15) THEN
-      BEGIN
               WRITE('and ');
               write_high_teens(the_number);
             END
